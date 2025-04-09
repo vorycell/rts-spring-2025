8bb955ad70064f9ad63f23822fe8751508541924
Фёдор <fedbondar2003@gmail.com>
2025-04-10 00:59:26 +0400

    init

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,33 @@
-void setup() {
+struct Signal {
+  uint8_t channel;
+  uint32_t delayTime;
+  volatile uint32_t lastUpdate;
+  bool status;
+};
 
+Signal signals[] = {
+  {3,  10000, 0, LOW},
+  {5,   1000, 0, LOW},
+  {6,    500, 0, LOW},
+  {9,    100, 0, LOW},
+  {10,    50, 0, LOW}
+};
+constexpr uint8_t totalSignals = sizeof(signals) / sizeof(Signal);
+
+void setup() {
+  for (uint8_t idx = 0; idx < totalSignals; idx++) {
+    pinMode(signals[idx].channel, OUTPUT);
+  }
 }
 
 void loop() {
+  uint32_t timeNow = micros();
 
+  for (uint8_t idx = 0; idx < totalSignals; idx++) {
+    if (timeNow - signals[idx].lastUpdate >= signals[idx].delayTime) {
+      signals[idx].lastUpdate = timeNow;
+      signals[idx].status = !signals[idx].status;
+      digitalWrite(signals[idx].channel, signals[idx].status);
+    }
+  }
 }
--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,7 +1,56 @@
-void setup() {
+volatile uint32_t sumIntervals = 0;
+volatile uint32_t sumSquaredIntervals = 0;
+volatile uint32_t lastTimestamp = 0;
+volatile unsigned int measurementCount = 0;
+const unsigned int maxSamples = 42;
 
+void setup() {
+  Serial.begin(115200);
+  pinMode(21, INPUT);
+  attachInterrupt(digitalPinToInterrupt(21), onPinChange, CHANGE);
+}
+
+void onPinChange() {
+  unsigned long currentTime = micros();
+
+  if (lastTimestamp) {
+    unsigned long delta = currentTime - lastTimestamp;
+
+    sumIntervals += delta;
+    sumSquaredIntervals += delta * delta;
+    measurementCount++;
+  }
+
+  lastTimestamp = currentTime;
 }
 
 void loop() {
+  if (measurementCount >= maxSamples) {
+    noInterrupts();
+    uint32_t intervalSumSnapshot = sumIntervals;
+    uint32_t squaredIntervalSumSnapshot = sumSquaredIntervals;
+    unsigned int sampleSnapshot = measurementCount;
+    sumIntervals = 0;
+    sumSquaredIntervals = 0;
+    measurementCount = 0;
+    interrupts();
 
+    uint32_t avgInterval = intervalSumSnapshot / sampleSnapshot;
+    uint32_t variance = (squaredIntervalSumSnapshot / sampleSnapshot) - (avgInterval * avgInterval);
+    uint32_t stdDeviation = sqrt(variance);
+
+    Serial.print("Mean (us): ");
+    Serial.print(avgInterval);
+    Serial.print(", Accumulated Squared Interval (us): ");
+    Serial.print(squaredIntervalSumSnapshot);
+    Serial.print(", Sample Count: ");
+    Serial.print(sampleSnapshot);
+    Serial.print(", Mean^2 (us): ");
+    Serial.print(avgInterval * avgInterval);
+    Serial.print(", Variance (us): ");
+    Serial.print(variance);
+    Serial.print(", Standard Deviation (us): ");
+    Serial.print(stdDeviation);
+    Serial.println("");
+  }
 }
